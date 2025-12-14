2c7574e0acbcbb9fe6e8bbcc3b7f464eaef8363d
SebastianCatunta <sebascatunta7@gmail.com>
2025-12-14 00:05:57 -0400

    avance pantalla agregar habitos

--- app/src/main/java/com/example/supersexiboys/HabitosActivity.kt
+++ app/src/main/java/com/example/supersexiboys/HabitosActivity.kt
@@ -1,20 +1,31 @@
 package com.example.supersexiboys
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.supersexiboys.databinding.ActivityHabitosBinding
 
 class HabitosActivity : AppCompatActivity() {
+    val context: Context = this
+    private lateinit var binding: ActivityHabitosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_habitos)
+        binding = ActivityHabitosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.nuevoHabitoButton.setOnClickListener {
+            val cambioAAgregarHabito: Intent = Intent(context, AgregandoHabitosActivity::class.java)
+            startActivity(cambioAAgregarHabito)
         }
     }
 }
